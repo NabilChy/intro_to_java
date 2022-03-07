65251e4ec4ab674732b27fb2979bdb5193ce08b9
Daiyan Chowdhury <daiyanrahmanchy@gmail.com>
2022-03-07 18:17:22 -0500

    Pattern functions

---
+++ src/testfunction/patternfunctions/PatternPrint6.java
@@ -0,0 +1,41 @@
+package testfunction.patternfunctions;
+
+public class PatternPrint6 {
+    public static void PrintingPattern6(int n){
+        int i, j;
+        int m = 1;
+
+        for (i = 65; i <= n + 65; i++){
+            char a = (char)i;
+            System.out.print(a);
+            if(i == n + 65){
+                for(j = n - 1 + 65; j >= 65; j--){
+                    a = (char)j;
+                    System.out.print(a);
+                }
+            }
+        }
+        System.out.println();
+        for(i = n; i > 0; i--){
+            for(j = 65; j <= i + 64; j++){
+                char a =(char)j;
+                System.out.print(a);
+            }
+
+            for(j = 1; j <= m; j++){
+                System.out.print(" ");
+            }
+            m = m + 2;
+
+            for(j = i + 64; j >= 65; j--){
+                char a = (char)j;
+                System.out.print(a);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        PrintingPattern6(5);
+    }
+}
