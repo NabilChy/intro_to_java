09444db7467838474967187344074b9947123a1e
Daiyan Chowdhury <daiyanrahmanchy@gmail.com>
2022-03-07 01:42:15 -0500

    Function exercise

---
+++ src/testfunction/patternfunctions/PatternPrint.java
@@ -0,0 +1,22 @@
+package testfunction.patternfunctions;
+
+public class PatternPrint {
+    public static void PrintingPattern(int n){
+        int i, j;
+
+        for (i = 1; i <= n; i++){
+            for (j = 1; j <= n; j++){
+                if(i%2 != 0){
+                    System.out.print("*");
+                }else{
+                    System.out.print("#");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        PrintingPattern(5);
+    }
+}
