d10f379317706cc6bf28d335406173cd4fd0e453
Daiyan Chowdhury <daiyanrahmanchy@gmail.com>
2022-03-07 01:28:26 -0500

    First function

---
+++ src/testfunction/PatternPrint.java
@@ -0,0 +1,22 @@
+package testfunction;
+
+public class PatternPrint {
+    public static void PrintingPattern(int value){
+        for (int i = 1; i <= value; i++){
+            for (int j = 1; j <= i; j++){
+                if(i%2 != 0){
+                    System.out.print("*");
+                }else{
+                    System.out.print("#");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        PrintingPattern(10);
+    }
+}
+
+
--- src/testfunction/PrintTriangle.java
+++
@@ -1,4 +0,0 @@
-package testfunction;
-
-public class PrintTriangle {
-}
