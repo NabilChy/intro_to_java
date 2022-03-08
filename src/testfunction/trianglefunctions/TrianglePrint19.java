d4991799f5056fd849ef7a08e133a3b262dfddda
Daiyan Chowdhury <daiyanrahmanchy@gmail.com>
2022-03-08 18:46:18 -0500

    Triangle Function

--- src/testfunction/trianglefunctions/TrianglePrint19.java
+++ src/testfunction/trianglefunctions/TrianglePrint19.java
@@ -2,7 +2,27 @@ package testfunction.trianglefunctions;
 
 public class TrianglePrint19 {
     public static void PrintingTriangle19(int n){
-        int i,j,k;
+        int i, j ,k ;
+
+        for(i = 1; i <= n; i++){
+            for(j = i; j < n; j++){
+                System.out.print(" ");
+            }
+            for(k = 1; k <= i; k++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+
+        for(i = n - 1; i >= 1; i--){
+            for(j = i; j <= n - 1; j++){
+                System.out.print(" ");
+            }
+            for(k = 1; k <= i; k++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
 
     }
 
