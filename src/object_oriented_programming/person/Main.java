29c1cf414f87f21bea56bf5fd0fe351b9c37a4ac
Daiyan Chowdhury <daiyanrahmanchy@gmail.com>
2022-03-24 23:04:30 -0400

    OOP

---
+++ src/object_oriented_programming/person/Main.java
@@ -0,0 +1,40 @@
+package object_oriented_programming.person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person[] personArray = new Person[10];
+        personArray[0] = new Person("Andrew",18,  5.2, 197, 'M', "Upper Darby");
+        personArray[1] = new Person("Tom",50,  5.5, 210, 'M', "Outer Wall Slum");
+        personArray[2] = new Person("Martha",45,  4.2, 180, 'F', "Inner Wall");
+        personArray[3] = new Person("Eren",12,  6.7, 250, 'M', "Middle Wall");
+        personArray[4] = new Person("Mikasa",13,  6.11, 170, 'F', "Lost City");
+        personArray[5] = new Person("Lucy",75,  4.8, 130, 'F', "Magnolia");
+        personArray[6] = new Person("Gray",20,  7.9, 270, 'M', "Magnolia");
+        personArray[7] = new Person("Piper",32,  9.0, 300, 'F', "Kentucky");
+        personArray[8] = new Person("Kiddo",10,  3.5, 80, 'M', "Shire");
+        personArray[9] = new Person("Armin",21,  6.0, 110, 'M', "Gondor");
+
+        // Prints out the female members.
+        for (int i = 0; i < personArray.length; i++){
+            if (personArray[i].gender == 'F'){
+                System.out.println(personArray[i]);
+            }
+        }
+
+        //Sorts the array in ascending order according to age. Returns the sorted array
+        Person.sortAscendingByAge(personArray); // Gives Null-pointer-exception when the array is not full
+
+        //Prints the Sorted array;
+        System.out.println();
+        System.out.println("Sorted");
+        for (int i = 0; i < personArray.length; i++){
+            System.out.println(personArray[i]);
+        }
+
+        //print youngest members information
+        System.out.println("Youngest member: \n" + personArray[0]);
+
+        //print eldest member information
+        System.out.println("Eldest member: \n" + personArray[9]); //Will not work if array is not full.
+    }
+}
---
+++ src/object_oriented_programming/person/Person.java
@@ -0,0 +1,94 @@
+package object_oriented_programming.person;
+
+public class Person {
+    public String name;
+    public int age;
+    private double height;
+    private double weight;
+    public char gender;
+    public String address;
+
+    public Person(String name, int age, double height, double weight, char gender, String address) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.gender = gender;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", gender=" + gender +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public static Person[] sortAscendingByAge(Person[] arr){
+        Person[] temp = new Person[1];
+        for (int i = 0; i < arr.length; i++){
+            for(int j = i+1; j < arr.length; j++) {
+                if (arr[j].age < arr[i].age) {
+                    temp[0] = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp[0];
+                }
+            }
+        }
+        return arr;
+    }
+
+}
